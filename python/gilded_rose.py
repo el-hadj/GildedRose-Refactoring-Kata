2b3042c83cc7f069dffdd711c0777ee36bd09ed8
el-hadj <elhadj.balde@etu.u-pec.fr>
2025-07-04 19:20:20 +0200

    [feat] Reformating item class

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -46,10 +46,10 @@ class GildedRose(object):
 
 
 class Item:
-    def __init__(self, name, sell_in, quality):
+    def __init__(self, name: str, sell_in: int, quality: int):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
 
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    def __str__(self):
+        return f"(Name = {self.name}, SellIn = {self.sell_in}, Quality = {self.quality})"

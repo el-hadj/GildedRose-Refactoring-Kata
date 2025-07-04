fc3890dd3be0467475a99cfdac9e8d0e034ef639
el-hadj <elhadj.balde@etu.u-pec.fr>
2025-07-04 19:52:18 +0200

    [feat] Updating normal item with success

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -58,23 +58,10 @@ def update_sulfuras_quality(item: Item):
     pass
 
 def update_normal_quality(item: Item):
-    if item.quality > 0:
-        if item.name != "Sulfuras, Hand of Ragnaros":
-            item.quality = item.quality - 1
-
-    if item.name != "Sulfuras, Hand of Ragnaros":
-        item.sell_in = item.sell_in - 1
-    if item.sell_in < 0:
-        if item.name != "Aged Brie":
-            if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                item.quality = item.quality - item.quality
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
+    item.sell_in = item.sell_in - 1
+    if  (item.sell_in < 0 and item.quality > 0) \
+        or item.quality > 0:
+        item.quality = item.quality - 1
 
 def update_conjured_quality(item: Item):
     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
